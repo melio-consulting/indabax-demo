08be07c9b389f3eaf4633473ae18a5f5e950baf2
Merelda Wu <mereldawu@gmail.com>
2022-12-21 18:41:55 +0200

    Echo whoami

--- gcp-deploy.sh
+++ gcp-deploy.sh
@@ -5,10 +5,16 @@ REGION="europe-west3"
 PROJECT_ID=$(gcloud config get-value project)
 DOCKER_IMG="gcr.io/$PROJECT_ID/$NAME"
 
+gcloud config 
+
 # Check whoami
 echo "1"
 echo $(gcloud config list account)
 echo "2"
+echo $(gcloud auth list)
+echo "3"
+echo $(gcloud config list)
+echo "4"
 
 # Build Docker image using Cloud Build
 gcloud builds submit --tag $DOCKER_IMG --timeout=500s
